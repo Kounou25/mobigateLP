317106a3597b684e1118f25e64c6052eec9537cf
Kounou Gnimassou Gilbert <kounougilbert@MacBook-Air-de-Kounou.local>
2024-10-01 10:18:41 +0100

    first commit

---
+++ components/Features/featuresData.tsx
@@ -0,0 +1,48 @@
+import { Feature } from "@/types/feature";
+
+const featuresData: Feature[] = [
+  {
+    id: 1,
+    icon: "/images/icon/icon-01.svg",
+    title: "Suivi et Gestion des Dispositifs Mobiles",
+    description:
+      "Mobigate permet de surveiller l’utilisation de chaque appareil mobile dans ton parc. Que ce soit pour vérifier les applications les plus utilisées ou pour assurer que les appareils respectent les politiques de l'entreprise, tu as une vue complète et actualisée.",
+  },
+  {
+    id: 2,
+    icon: "/images/icon/icon-02.svg",
+    title: "Contrôle à Distance des Appareils",
+    description:
+      "Mobigate te permet d’intervenir à distance sur les appareils de la flotte. Par exemple, tu peux verrouiller ou réinitialiser un appareil en cas de perte ou de vol.",
+  },
+  {
+    id: 3,
+    icon: "/images/icon/icon-03.svg",
+    title: "Notifications et Alertes Automatisées",
+    description:
+      "Configure des alertes pour être notifié en cas d'événements spécifiques, tels que la désinstallation d'une application critique ou une utilisation anormale des données.",
+  },
+  {
+    id: 4,
+    icon: "/images/icon/icon-04.svg",
+    title: "Gestion des Mises à Jour",
+    description:
+      "Mobigate permet de planifier et de déployer les mises à jour logicielles ou du système d’exploitation de manière centralisée pour tout le parc.",
+  },
+  {
+    id: 5,
+    icon: "/images/icon/icon-05.svg",
+    title: "Sécurisation des Données",
+    description:
+      "Garantit la sécurité des informations sur chaque appareil en appliquant des règles de sécurité comme le cryptage des données, l’authentification renforcée ou encore la gestion des permissions d’accès.",
+  },
+  {
+    id: 6,
+    icon: "/images/icon/icon-06.svg",
+    title: "Assistance et Support Personnalisés",
+    description:
+      "Mobigate offre un service d’assistance technique dédié pour t’accompagner dans l’installation, la configuration et l’utilisation de la solution.",
+  },
+];
+
+export default featuresData;
