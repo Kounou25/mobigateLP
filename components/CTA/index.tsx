0e293a7a357b39d6c1dbbef51502d7edef69fba1
Kounou Gnimassou Gilbert <kounougilbert@MacBook-Air-de-Kounou.local>
2024-10-01 12:25:36 +0100

    ajout des temoignages personnalises

--- components/CTA/index.tsx
+++ components/CTA/index.tsx
@@ -29,7 +29,7 @@ const CTA = () => {
               className="animate_left md:w-[70%] lg:w-1/2"
             >
               <h2 className="mb-4 w-11/12 text-3xl font-bold text-black dark:text-white xl:text-sectiontitle4">
-                Rejoignez MobiGate des maintenant !
+                Essayez MobiGate des maintenant !
               </h2>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
--- components/Testimonial/testimonialData.ts
+++ components/Testimonial/testimonialData.ts
@@ -5,34 +5,34 @@ import { Testimonial } from "@/types/testimonial";
 export const testimonialData: Testimonial[] = [
   {
     id: 1,
-    name: "Devid Smith",
-    designation: "Founter @democompany",
+    name: "David Kouma",
+    designation: "Responsable IT @TechLogistics",
     image: image1,
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+      "Mobigate a complètement transformé notre façon de gérer notre parc mobile. Le suivi en temps réel et la gestion à distance nous ont permis de sécuriser nos appareils et d'optimiser nos ressources. C’est un outil indispensable pour toute entreprise avec une flotte mobile.",
   },
   {
     id: 2,
-    name: "Jhon Abraham",
-    designation: "Founter @democompany",
+    name: "Amina Diallo",
+    designation: "Directrice des Opérations @TransSahara",
     image: image2,
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+      "Grâce à Mobigate, nous avons réduit de moitié les incidents de perte d’appareils. Le suivi GPS et les alertes automatiques nous permettent de réagir rapidement. C’est un véritable game changer pour notre entreprise.",
   },
   {
     id: 3,
-    name: "Devid Smith",
-    designation: "Founter @democompany",
+    name: "Jean-Pierre Ndao",
+    designation: "Chef de Projet @EduConnect",
     image: image1,
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+      "Nous avons enfin trouvé une solution adaptée à la gestion de nos appareils mobiles dans plusieurs régions. Mobigate nous offre une vue complète sur l'utilisation des dispositifs et nous aide à mieux allouer nos ressources technologiques.",
   },
   {
     id: 4,
-    name: "Jhon Abraham",
-    designation: "Founter @democompany",
+    name: "Fatou Diop",
+    designation: "Responsable des Opérations @FleetSecure",
     image: image2,
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+      "La capacité de Mobigate à gérer les mises à jour à distance a fait une énorme différence pour notre équipe. Nous n’avons plus besoin d'intervenir manuellement sur chaque appareil, ce qui nous fait gagner un temps précieux.",
   },
 ];
